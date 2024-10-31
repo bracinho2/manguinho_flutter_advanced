d7a3a967d2da64a3c8f85beda4ff0650b440950c
Alexandre | BrAcInhO <bracinho2@hotmail.com>
2024-10-31 07:06:37 -0300

    move client http spy to its own file and LoadNextHttpRepoTest to its own folder

--- test/infra/api/repositories/load_next_event_http_repo_test.dart
+++ test/infra/api/repositories/load_next_event_http_repo_test.dart
@@ -1,84 +1,10 @@
-import 'dart:convert';
-
-import 'dart:typed_data';
-
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
+
 import 'package:manguinho_flutter_advanced/domain/entities/domain_error.dart';
 import 'package:manguinho_flutter_advanced/infra/api/repositories/load_next_event_http_repo.dart';
 
-import '../../helpers/fakes.dart';
-
-class HttpClientSpy implements Client {
-  String? method;
-  int callsCount = 0;
-  String? url;
-  Map<String, String>? headers;
-  String responseJson = '';
-  int statusCode = 200;
-
-  void simulateBadRequestError() => statusCode = 400;
-  void simulateUnauthorizedError() => statusCode = 401;
-  void simulateForbiddenError() => statusCode = 403;
-  void simulateNotFoundError() => statusCode = 404;
-  void simulateServerError() => statusCode = 500;
-
-  @override
-  Future<Response> get(Uri url, {Map<String, String>? headers}) async {
-    method = 'get';
-    this.url = url.toString();
-    this.headers = headers;
-    callsCount++;
-    return Response(responseJson, statusCode);
-  }
-
-  @override
-  void close() {}
-
-  @override
-  Future<Response> delete(Uri url,
-      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> head(Uri url, {Map<String, String>? headers}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> patch(Uri url,
-      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> post(Uri url,
-      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Response> put(Uri url,
-      {Map<String, String>? headers, Object? body, Encoding? encoding}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<String> read(Uri url, {Map<String, String>? headers}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Uint8List> readBytes(Uri url, {Map<String, String>? headers}) {
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<StreamedResponse> send(BaseRequest request) {
-    throw UnimplementedError();
-  }
-}
+import '../../../helpers/fakes.dart';
+import '../clients/http_client_spy.dart';
 
 void main() {
   late String groupId;
