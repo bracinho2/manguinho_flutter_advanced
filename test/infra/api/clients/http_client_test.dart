a9486d75f0a0a6fa5947f54a624715e5786125ac
Alexandre | BrAcInhO <bracinho2@hotmail.com>
2024-11-01 06:56:37 -0300

    ensure HttpClient request with correct method

---
+++ test/infra/api/clients/http_client_test.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+
+import 'client_spy.dart';
+
+class HttpClient {
+  final Client client;
+  HttpClient({required this.client});
+  Future<void> get() async {
+    client.get(Uri());
+  }
+}
+
+void main() {
+  test('shoud request with correct method', () async {
+    final client = ClientSpy();
+    final sut = HttpClient(client: client);
+    await sut.get();
+    expect(client.method, 'get');
+    expect(client.callsCount, 1);
+  });
+}
