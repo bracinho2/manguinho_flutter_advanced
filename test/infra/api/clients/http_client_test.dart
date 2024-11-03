a3ca266285eb26c5df80bdb3c175a2ebe948de07
Alexandre | BrAcInhO <bracinho2@hotmail.com>
2024-11-03 06:53:34 -0300

    ensure HttpClient request with invalid params

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -77,5 +77,10 @@ void main() {
       await sut.get(url: url, params: {'p1': 'v1', 'p2': null});
       expect(client.url, 'http://anyurl.com/v1');
     });
+    test('should requirest with invalid params', () async {
+      url = 'http://anyurl.com/:p1/:p2';
+      await sut.get(url: url, params: {'p3': 'v3'});
+      expect(client.url, 'http://anyurl.com/:p1/:p2');
+    });
   });
 }
