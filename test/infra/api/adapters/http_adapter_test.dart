a7efaf6724dfd661fd22d9b41a63f3e42ee7fcb9
Alexandre | BrAcInhO <bracinho2@hotmail.com>
2024-11-04 19:10:14 -0300

    move HttpAdapter to its own file

--- test/infra/api/adapters/http_adapter_test.dart
+++ test/infra/api/adapters/http_adapter_test.dart
@@ -1,72 +1,15 @@
-import 'dart:convert';
-
-import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart';
+
 import 'package:manguinho_flutter_advanced/domain/entities/domain_error.dart';
+import 'package:manguinho_flutter_advanced/infra/api/adapters/http_adapter.dart';
 import 'package:manguinho_flutter_advanced/infra/types/json.dart';
 
 import '../../../helpers/fakes.dart';
-import 'client_spy.dart';
-
-class HttpClient {
-  final Client client;
-  HttpClient({required this.client});
-
-  Future<T?> get<T>({
-    required String url,
-    Map<String, String>? headers,
-    Map<String, String?>? params,
-    Map<String, String>? queryString,
-  }) async {
-    final allHeaders = (headers ?? {})
-      ..addAll(
-          {'content-type': 'application/json', 'accept': 'application/json'});
-    final uri = _buildUri(url: url, params: params, queryString: queryString);
-    final response = await client.get(uri, headers: allHeaders);
-
-    switch (response.statusCode) {
-      case 200:
-        {
-          if (response.body.isEmpty) return null;
-          final data = jsonDecode(response.body);
-          return (T == JsonArr)
-              ? data.map<Json>((e) => e as Json).toList()
-              : data;
-        }
-      case 204:
-        return null;
-      case 401:
-        throw DomainError.sessionExpired;
-      default:
-        throw DomainError.unexpected;
-    }
-  }
-
-  Uri _buildUri(
-      {required String url,
-      Map<String, String?>? params,
-      Map<String, String>? queryString}) {
-    url = params?.keys
-            .fold(
-                url,
-                (result, key) =>
-                    result.replaceFirst(':$key', params[key] ?? ''))
-            .removeSuffix('/') ??
-        url;
-
-    url = (queryString?.keys
-            .fold('$url?', (result, key) => '$result$key=${queryString[key]}&')
-            .removeSuffix('&')) ??
-        url;
-
-    return Uri.parse(url);
-  }
-}
+import '../clients/client_spy.dart';
 
 void main() {
   late ClientSpy client;
-  late HttpClient sut;
+  late HttpAdapter sut;
   late String url;
 
   setUp(() {
@@ -77,7 +20,7 @@ void main() {
       "key2": "value2"
     }
     ''';
-    sut = HttpClient(client: client);
+    sut = HttpAdapter(client: client);
     url = anyString();
   });
 
